d252523ca111d1324288a90ad24ca1c1e8ea1fc8
adahuangsx <huangsx_97@foxmail.com>
2020-08-13 20:37:59 -0400

    implementing MySQL connection

--- src/db/mysql/MySQLConnection.java
+++ src/db/mysql/MySQLConnection.java
@@ -1,17 +1,39 @@
 package db.mysql;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Set;
 
 import db.DBConnection;
 import entity.Item;
+import external.TicketMasterAPI;
 
 public class MySQLConnection implements DBConnection {
 
+	private Connection conn;
+	
+	public MySQLConnection() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver").getConstructor().newInstance();
+			conn = DriverManager.getConnection(MySQLDBUtil.URL);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
 	@Override
 	public void close() {
 		// TODO Auto-generated method stub
-
+		if (conn != null) {
+			try {
+				conn.close();
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 	@Override
@@ -46,13 +68,56 @@ public class MySQLConnection implements DBConnection {
 
 	@Override
 	public List<Item> searchItems(double lat, double lon, String term) {
-		// TODO Auto-generated method stub
-		return null;
+		TicketMasterAPI tmAPI = new TicketMasterAPI();
+		List<Item> items = tmAPI.search(lat, lon, term);
+		for (Item item : items) {
+			saveItem(item);
+		}
+		return items;
 	}
 
 	@Override
 	public void saveItem(Item item) {
-		// TODO Auto-generated method stub
+		if (conn == null) {
+			return; // send a message to front end
+		}		
+		try {
+			// SQL injection
+			// Example:
+			// SELECT * FROM users WHERE username = '<username>' AND password = '<password>';
+			//
+			// sql = "SELECT * FROM users WHERE username = '" + username + "'
+			//       AND password = '" + password + "'"
+			//
+			// username: aoweifjoawefijwaoeifj
+			// password: 123456' OR '1' = '1
+			//
+			// SELECT * FROM users WHERE username = 'aoweifjoawefijwaoeifj' AND password = '123456' OR '1' = '1'
+			String sql = "INSERT IGNORE INTO items VALUES (?, ?, ?, ?, ?, ?, ?)";
+			PreparedStatement stmt = conn.prepareStatement(sql);
+			stmt.setString(1, item.getItemId());
+			stmt.setString(2, item.getName());
+			stmt.setDouble(3, item.getRating());
+			stmt.setString(4, item.getAddress());
+			stmt.setString(5, item.getImageUrl());
+			stmt.setString(6, item.getUrl());
+			stmt.setDouble(7, item.getDistance());
+			stmt.execute();
+			
+			sql = "INSERT IGNORE INTO categories VALUES (?, ?)";
+			stmt = conn.prepareStatement(sql);
+			for (String category : item.getCategories()) {
+				stmt.setString(1, item.getItemId());
+				stmt.setString(2, category);
+				stmt.execute();
+				// difference between this and executeUpdate() is:
+				// the latter returns an integer, meaning the number of updated rows
+				// which is meaningless here.
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
 
 	}
 
